f4579c15387ab28e5b03bb7aa8357b3ec39a0a26
Denzel Witbooi <dwitbooi28@gmail.com>
2022-03-05 13:39:55 +0200

    add conditional statement

--- index.php
+++ index.php
@@ -15,9 +15,12 @@
                 $products['Car'] = 15000;
                 $products['iPhone'] = 1000;
                 $products['Toaster'] = 75;
+      echo "<h2>Items you can afford</h2>";
 
       foreach ($products as $key => $value) {
-        echo "<p>The " .$key. " costs ".$value. "</p>";
+        if ($value <= $credit) {
+            echo "<p>" .$key. "</p>";
+        }
       }
 
      ?>
